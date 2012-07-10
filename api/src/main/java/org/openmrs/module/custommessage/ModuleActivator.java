84f633bc5b6d11b6ed0e7f0e0d2ffa579e7497a1
mvorobey <vorobeymykola@gmail.com>
2012-07-10 21:47:32 +0300

    Preparing v1.2 release

--- api/src/main/java/org/openmrs/module/custommessage/ModuleActivator.java
+++ api/src/main/java/org/openmrs/module/custommessage/ModuleActivator.java
@@ -15,22 +15,22 @@ package org.openmrs.module.custommessage;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.Activator;
 import org.openmrs.web.taglib.OpenmrsMessageTag;
 
 /**
  * This class contains the logic that is run every time this module
  * is either started or shutdown
  */
-public class ModuleActivator extends BaseModuleActivator {
+public class ModuleActivator implements Activator {
 
 	private static final Log LOG = LogFactory.getLog(ModuleActivator.class);
 	
 	/**
-	 * @see org.openmrs.module.BaseModuleActivator#started()
+	 * @see org.openmrs.module.Activator#startup()
 	 */
 	@Override
-	public void started() {
+	public void startup() {
 		
 		// customize tag writer behavior for openmrs:message tag
 		OpenmrsMessageTag.setTagWriterBehavior(new CustomTagMessageWriterBehavior());
@@ -39,10 +39,10 @@ public class ModuleActivator extends BaseModuleActivator {
 	}
 	
 	/**
-	 * @see org.openmrs.module.BaseModuleActivator#stopped()
+	 * @see org.openmrs.module.Activator#shutdown()
 	 */
 	@Override
-	public void stopped() {
+	public void shutdown() {
 		
 		// reset openmrs:message tag writer behavior when module is stopped
 		OpenmrsMessageTag.setTagWriterBehavior(OpenmrsMessageTag.DEFAULT_WRITER_BEHAVIOUR);
