626efb35a93feb0fd5ccd163dfe3396f37fd785d
mvorobey <vorobeymykola@gmail.com>
2012-08-16 17:43:20 +0300

    CSTM-25 - Added velocity engine support for rendering extension point content

--- omod/src/main/java/org/openmrs/module/custommessage/extension/ToggleTranslateModeExtension.java
+++ omod/src/main/java/org/openmrs/module/custommessage/extension/ToggleTranslateModeExtension.java
@@ -13,9 +13,19 @@
  */
 package org.openmrs.module.custommessage.extension;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.log.CommonsLogLogChute;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.Extension;
@@ -29,6 +39,31 @@ import org.openmrs.web.WebConstants;
  * translate mode on/off. This extension is enabled by defining it in the config.xml file.
  */
 public class ToggleTranslateModeExtension extends HeaderIncludeExt {
+	
+	private static final Log LOG = LogFactory.getLog(ToggleTranslateModeExtension.class);
+	
+	/** Velocity engine to be used for generating extension point content from template */
+	private VelocityEngine velocityEngine;
+	
+	/** Contains velocity template string for rendering extension point content */
+	private static String contentTemplate;
+	
+	/** The name of file with velocity template for extension point content */
+	private static final String CONTENT_TEMPLATE_NAME = "extension-point-content.vm";
+	
+	static {
+		// We need to initialize velocity template at class loading time 
+		// in order to reduce I/O during runtime because this template will
+		// be used relatively often
+		try {
+	        contentTemplate = IOUtils.toString(ToggleTranslateModeExtension.class.getClassLoader().getResourceAsStream(CONTENT_TEMPLATE_NAME));
+        }
+        catch (IOException e) {
+	        LOG.error("Unable to read template to be used for rendering extension point content", e);
+	        // as fallback use empty string for content template
+	        contentTemplate = StringUtils.EMPTY;
+        }
+	}
 
 	/**
 	 * @see org.openmrs.module.Extension#getMediaType()
@@ -54,21 +89,23 @@ public class ToggleTranslateModeExtension extends HeaderIncludeExt {
 		// current user is granted to do messages customization
 		if (CustomMessageUtil.isInplaceCustomizationEnabled() && currentUser != null
 		        && currentUser.hasPrivilege(getRequiredPrivilege())) {
-			String contextPath = WebConstants.WEBAPP_NAME;
-			String translateModeStatus = currentUser.getUserProperty(
-			    CustomMessageConstants.USER_PROPERTY_TRANSLATE_MODE_ENABLED, "false");
-		    return String
-			        .format(
-			        	"<script src=\"/%s/dwr/interface/DWRCustomMessageService.js\"></script>\n" +
-					    "<script type=\"text/javascript\">\n var jQueryIsExisting = typeof(jQuery) != \"undefined\";\n</script>\n" + 
-			        	"<script src=\"/%s/moduleResources/custommessage/jquery-1.7.2.min.js\" type=\"text/javascript\"></script>\n" +
-			        	"<script type=\"text/javascript\">\n jQuery.noConflict(jQueryIsExisting);\n</script>" + 
-			            "<script src=\"/%s/moduleResources/custommessage/jquery.caret.js\" type=\"text/javascript\"></script>\n" +
-			        	"<script src=\"/%s/moduleResources/custommessage/jquery.jeditable-1.7.2.js\" type=\"text/javascript\"></script>\n" +
-			            "<script src=\"/%s/moduleResources/custommessage/openmrs-editable.js\" type=\"text/javascript\"></script>\n" +
-			            "<script type=\"text/javascript\">\njQuery(window).load(function() {\n\n  // handle translate mode properly\n  var translateMode = %s;\n    handleTranslateMode(translateMode);\n});\n</script>\n" +
-			            "<input type=\"button\" id=\"translateButton\"/>",
-			            contextPath, contextPath, contextPath, contextPath, contextPath, translateModeStatus);
+			
+			initializeVelocity();
+			
+			VelocityContext velocityContext = new VelocityContext();
+			velocityContext.put("contextPath", WebConstants.WEBAPP_NAME);
+			velocityContext.put("translateModeStatus", currentUser.getUserProperty(
+			    CustomMessageConstants.USER_PROPERTY_TRANSLATE_MODE_ENABLED, "false"));
+			
+			String overridenContent = null;
+			try {
+				StringWriter writer = new StringWriter();
+				velocityEngine.evaluate(velocityContext, writer, this.getClass().getName(), contentTemplate);
+				overridenContent = writer.toString();
+			} catch (Exception e) {
+				LOG.error("Error evaluating template when getting overwritten content for extesion point, because of error:", e);
+			}
+		    return overridenContent;
 		} else {
 			return bodyContent;
 		}
@@ -81,5 +118,26 @@ public class ToggleTranslateModeExtension extends HeaderIncludeExt {
     public List<String> getHeaderFiles() {
 	    return new ArrayList<String>(0);
     }
+	
+	/**
+	 * A utility method to initialize Velocity. This could be called in the constructor, but putting
+	 * it in a separate method like this allows for late-initialization only when actual using of
+	 * this extension point occurred at first time.
+	 */
+	private void initializeVelocity() {
+		if (velocityEngine == null) {
+			velocityEngine = new VelocityEngine();
+			
+			velocityEngine.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS,
+			    "org.apache.velocity.runtime.log.CommonsLogLogChute");
+			velocityEngine.setProperty(CommonsLogLogChute.LOGCHUTE_COMMONS_LOG_NAME, "custommessage_velocity");
+			try {
+				velocityEngine.init();
+			}
+			catch (Exception e) {
+				LOG.error("velocity init failed", e);
+			}
+		}
+	}
 
 }
